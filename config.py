9aafd8d09f3903092a1f352d3d75a3befc00ad9d
ZeeXDev <otakukingcey@gmail.com>
2026-01-27 19:26:54 +0000

    C3

--- config.py
+++ config.py
@@ -110,7 +110,7 @@ CMD_TXT = """<blockquote><b>» Commandes administrateur :</b></blockquote>
 # ==========================================
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "False" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #Mettez True si vous voulez empêcher le transfert de fichiers depuis le bot
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'False'
 
 BOT_STATS_TEXT = "<b>TEMPS DE FONCTIONNEMENT DU BOT</b>\n{uptime}"
